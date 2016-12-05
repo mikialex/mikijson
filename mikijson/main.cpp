e4b9f2e0816bf2e1c6e4db0c9fcd46303ce02c6a
MikiAlex <18516340862@163.com>
2016-12-05 22:52:28 +0800

    Initial Commit

---
+++ mikijson/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mikijson
+//
+//  Created by MikiAlex on 16/12/5.
+//  Copyright © 2016年 MikiAlex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
